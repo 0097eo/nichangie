59376bc1aaa43c8e737430b6b91e187f94fa845d
Emmanuel <emmanuelokello294@gmail.com>
2024-06-16 08:33:04 +0300

    added header

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Hello World</h1>
+      <p>This is my fundraiser app</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Header/Header.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+import logo from '../Assets/Logos/logo-no-background.png'
+import './Header.css'
+
+const Header = () => {
+
+    const [searchInput, setSearchInput] = useState('')
+
+    const handleChange = (e) => {
+        setSearchInput(e.target.value)
+    }
+
+  return (
+    <>
+    <nav className='navbar'>
+            <img src={logo} alt='logo'></img>
+        <ul>
+            <li>Start Fundraiser</li>
+            <li>About us</li>
+        </ul>
+    </nav>
+        <h2>Fundraising Made Simple</h2>
+    <div className='searchbar'>
+        <input type='text'
+         placeholder='find fundraiser'
+         value={searchInput}
+         onChange={handleChange}
+        ></input>
+    </div>
+    </>
+  )
+}
+
+export default Header
